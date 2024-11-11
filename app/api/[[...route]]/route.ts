6a4420bf9fcd64998f7886846a9e0d19ddf7782e
jppctech <dilipmorigaon@gmail.com>
2024-11-11 17:03:38 +0530

    update

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,15 @@
-import { Hono } from 'hono'
-import { handle } from "hono/vercel"
-import contact from "./contact"
+import { handle } from "hono/vercel";
+import { Hono } from "hono";
 
+import contact from "./contact"
 export const runtime = "edge"
 
-const app = new Hono().basePath("/api")
+const app = new Hono().basePath("/api");
 
 const routes = app
-    .route("/", contact)
+    .route("/accounts", contact);
 
 export const GET = handle(app)
 export const POST = handle(app)
 
-export type AppType = typeof routes;
+export type Apptype = typeof routes;
